1cfff2b932a08354ca5acc01c07484b9e1e75905
Theodore Keloglou <theodore.keloglou@wagestream.co.uk>
2020-05-15 15:21:31 +0100

    add biscuit

---
+++ biscuit_maker/biscuit.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class BiscuitStates(Enum):
+    NONEXISTENT = "NONEXISTENT"
+    DOLLOP = "DOLLOP"
+    STAMPED = "STAMPED"
+    BAKED_SEMI = "BAKED_SEMI"
+    BAKED_FULLY = "BAKED_FULLY"
+
+
+class Biscuit(object):
+    """Biscuit base class"""
+
+    def __init__(self):
+        """"""
+        self._state = BiscuitStates.NONEXISTENT
+
+    def process(self, new_state, **args):
+        """Base interface for changing the state of biscuit."""
+        self._state = new_state
+
+    def get_state(self):
+        """Base interface for reading current biscuit."""
+        return self._state
